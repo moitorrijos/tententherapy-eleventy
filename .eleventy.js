d06f1a5adef153d254fcf8bae2934be0c978e8a4
Juan Moises Torrijos <juanmtorrijos@gmail.com>
2022-02-08 22:49:07 -0500

    Cambios pedidos por cliente.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,30 @@
+const sass = require('sass');
+const postcss = require('postcss');
+const postcssPresetEnv = require('postcss-preset-env');
+const postcssCsso = require('postcss-csso');
+const fs = require('fs-extra');
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget("./sass/");
+  eleventyConfig.on("beforeBuild", () => {
+    let result = sass.renderSync({
+      file: "sass/main.sass",
+      sourceMap: false,
+      outputStyle: "compressed",
+    });
+    console.log("SASS compiled");
+  
+    // Optimize and write file with PostCSS
+    let css = result.css.toString();
+    postcss([postcssPresetEnv, postcssCsso])
+      .process(css, { from: "main.css", to: "css/main.css" })
+      .then((result) => {
+        fs.outputFile("_site/css/main.css", result.css, (err) => {
+          if (err) throw err;
+          console.log("CSS optimized");
+        });
+      });
+  });
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
