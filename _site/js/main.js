854afe90f6dd89232053f6669027a045eafae43a
Juan Moises Torrijos <juanmtorrijos@gmail.com>
2022-03-10 09:09:27 -0500

    Adds honeypot to form. Improves menu toggle button functionality.

--- _site/js/main.js
+++ _site/js/main.js
@@ -2,13 +2,9 @@ window.addEventListener('load', () => {
   quicklink.listen();
   const menuToggle = document.querySelector(".menu-toggle")
   const menu = document.querySelector("ul.mainmenu")
-  menuToggle.addEventListener('click', function(event){
+  menuToggle.addEventListener('click', function(event) {
     const { target } = event;
-    if ( target.nodeName === 'SPAN' ) {
-        target.offsetParent.classList.toggle('open');
-    } else {
-        target.classList.toggle('open');
-    }
     menu.classList.toggle('visible-menu');
-})
+    target.classList.toggle('open');
+  })
 });
